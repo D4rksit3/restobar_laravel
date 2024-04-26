d1f2e238543079a7222496581b2d24fd1700cf21
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2024-04-26 13:36:34 -0500

    env

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('google_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            
         });
     }
 
